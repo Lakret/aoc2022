49aa361988c2b19d54eeca1fe6ec8a6d0e13b479
lakret <Rakuretu@gmail.com>
2022-12-21 15:49:20 +0100

    enough livebook

--- d21.exs
+++ d21.exs
@@ -1,8 +1,3 @@
-# Day 21
-
-## Section
-
-```elixir
 defmodule D21 do
   def parse_input(path) do
     File.read!(path)
@@ -67,6 +62,37 @@ defmodule D21 do
     end
   end
 
+  def topological_order(input, stop_monkey) do
+    {no_deps, graph} =
+      Enum.reduce(input, {MapSet.new(), %{}}, fn
+        {name, val}, {no_deps, graph} when is_number(val) ->
+          {MapSet.put(no_deps, name), graph}
+
+        {name, {_op, name1, name2}}, {no_deps, graph} ->
+          {no_deps, Map.put(graph, name, [name1, name2])}
+      end)
+
+    topological_order(no_deps, graph, stop_monkey, [])
+  end
+
+  defp topological_order(no_deps, graph, stop_monkey, ordering) do
+    ordering = ordering ++ MapSet.to_list(no_deps)
+
+    if MapSet.size(no_deps) == 0 || stop_monkey in no_deps do
+      ordering
+    else
+      {new_no_deps, graph} =
+        Enum.reduce(graph, {MapSet.new(), %{}}, fn {name, dependencies}, {new_no_deps, graph} ->
+          case Enum.filter(dependencies, fn dep -> dep not in no_deps end) do
+            [] -> {MapSet.put(new_no_deps, name), graph}
+            dependencies -> {new_no_deps, Map.put(graph, name, dependencies)}
+          end
+        end)
+
+      topological_order(new_no_deps, graph, stop_monkey, ordering)
+    end
+  end
+
   def p1(input) do
     eval(input, "root") |> round()
   end
@@ -90,26 +116,27 @@ defmodule D21 do
     end)
   end
 end
-```
 
-```elixir
 input = D21.parse_input("inputs/d21")
 test_input = D21.parse_input("inputs/d21_test")
-```
 
-```elixir
 D21.p1(test_input) == 152
-```
-
-```elixir
 D21.p1(input) == 291_425_799_367_130
-```
 
-```elixir
+:timer.tc(fn -> D21.p1(test_input) end)
+:timer.tc(fn -> D21.p1(input) end)
+
+D21.topological_order(test_input, "root")
+test_input["root"]
+D21.topological_order(test_input, "pppw")
+D21.topological_order(test_input, "sjmn")
+
+D21.topological_order(input, "root") |> length()
+input["root"]
+D21.topological_order(input, "pgtp") |> length()
+D21.topological_order(input, "vrvh") |> length()
+D21.topological_order(input, "humn") |> length()
+
 D21.p2(test_input) == 301
-```
-
-```elixir
 # 30 seconds for 1000 iterations => way too slow
 D21.p2(input)
-```
