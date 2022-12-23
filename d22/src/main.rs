c05b4d5a77f6a0a4e0e2b11d97ca10eeb30c34a9
lakret <Rakuretu@gmail.com>
2022-12-23 22:28:27 +0100

    d22: p2 solved for test

--- d22/src/main.rs
+++ d22/src/main.rs
@@ -62,125 +62,6 @@ impl Into<usize> for Facing {
     }
 }
 
-/// left, right, up, and down ids are assigned to the adjacent face ids at the corresponding directions
-/// if the current face is "front" of the cube
-#[derive(Debug, Clone)]
-struct Face {
-    id: usize,
-    rows: Range<usize>,
-    cols: Range<usize>,
-    left: Transition,
-    right: Transition,
-    up: Transition,
-    down: Transition,
-}
-
-/// `swap` is `true` when we need to swap cols & rows coordinates, i.e. (2, 3) becomes (3, 2)
-/// `inv_row` is `true` when instead of arriving at a 0 relative row on the new face from 3 row on the previous face,
-///  we arrive at row 3 on the new face
-/// `inv_col` is `true` when instead of arriving at 0 relative column on the new face we arrive at column 3
-#[derive(Debug, Clone, Copy)]
-struct Transition {
-    face_id: usize,
-    facing: Facing,
-    swap: bool,
-    inv_row: bool,
-    inv_col: bool,
-}
-
-/// returns (new_face_id, new_facing, new_relative_row_id, new_relative_col_id)
-fn move_to_face(
-    faces: &[Face; 6],
-    size: usize,
-    from_face_id: usize,
-    facing: Facing,
-    row_id: usize,
-    col_id: usize,
-) -> (usize, Facing, usize, usize) {
-    let transition = match facing {
-        Left => faces[from_face_id].left,
-        Right => faces[from_face_id].right,
-        Down => faces[from_face_id].down,
-        Up => faces[from_face_id].up,
-    };
-
-    let mut row_id = row_id;
-    let mut col_id = col_id;
-
-    if transition.swap {
-        std::mem::swap(&mut row_id, &mut col_id);
-    }
-
-    if transition.inv_row {
-        row_id = size - row_id - 1;
-    }
-
-    if transition.inv_col {
-        col_id = size - col_id - 1;
-    }
-
-    (transition.face_id, transition.facing, row_id, col_id)
-}
-
-lazy_static! {
-    static ref TEST_FACES: [Face; 6] = [
-        Face {
-            id: 0,
-            rows: 0..4,
-            cols: 8..12,
-            left: Transition { face_id: 2, facing: Down, swap: true, inv_row: false, inv_col: false },
-            right: Transition { face_id: 5, facing: Left, swap: false, inv_row: true, inv_col: false },
-            up: Transition { face_id: 1, facing: Down, swap: false, inv_row: false, inv_col: true },
-            down: Transition { face_id: 3, facing: Down, swap: false, inv_row: true, inv_col: false },
-        },
-        Face {
-            id: 1,
-            rows: 4..8,
-            cols: 0..4,
-            left: Transition { face_id: 5, facing: Up, swap: true, inv_row: true, inv_col: true },
-            right: Transition { face_id: 2, facing: Right, swap: false, inv_row: false, inv_col: true },
-            up: Transition { face_id: 0, facing: Down, swap: false, inv_row: false, inv_col: true },
-            down: Transition { face_id: 4, facing: Up, swap: false, inv_row: false, inv_col: true }
-        },
-        Face {
-            id: 2,
-            rows: 4..8,
-            cols: 4..8,
-            left: Transition { face_id: 1, facing: Left, swap: false, inv_row: false, inv_col: true },
-            right: Transition { face_id: 3, facing: Right, swap: false, inv_row: false, inv_col: true },
-            up: Transition { face_id: 0, facing: Right, swap: true, inv_row: false, inv_col: false },
-            down: Transition { face_id: 4, facing: Right, swap: true, inv_row: true, inv_col: true }
-        },
-        Face {
-            id: 3,
-            rows: 4..8,
-            cols: 8..12,
-            left: Transition { face_id: 2, facing: Left, swap: false, inv_row: false, inv_col: true },
-            right: Transition { face_id: 5, facing: Down, swap: true, inv_row: true, inv_col: true },
-            up: Transition { face_id: 0, facing: Up, swap: false, inv_row: true, inv_col: false },
-            down: Transition { face_id: 4, facing: Down, swap: false, inv_row: true, inv_col: false }
-        },
-        Face {
-            id: 4,
-            rows: 8..12,
-            cols: 8..12,
-            left: Transition { face_id: 2, facing: Up, swap: true, inv_row: true, inv_col: true },
-            right: Transition { face_id: 5, facing: Right, swap: false, inv_row: false, inv_col: true },
-            up: Transition { face_id: 3, facing: Up, swap: false, inv_row: true, inv_col: false },
-            down: Transition { face_id: 1, facing: Up, swap: false, inv_row: false, inv_col: true }
-        },
-        Face {
-            id: 5,
-            rows: 8..12,
-            cols: 12..16,
-            left: Transition { face_id: 4, facing: Left, swap: false, inv_row: false, inv_col: true },
-            right: Transition { face_id: 0, facing: Left, swap: false, inv_row: true, inv_col: false },
-            up: Transition { face_id: 3, facing: Right, swap: true, inv_row: true, inv_col: true },
-            down: Transition { face_id: 1, facing: Right, swap: true, inv_row: true, inv_col: true }
-        }
-    ];
-}
-
 #[derive(Debug, Clone, Default)]
 struct Board {
     rows: Vec<Row>,
@@ -198,7 +79,6 @@ impl Board {
         for &step in &self.path {
             match step {
                 Step::Move { tiles } => {
-                    // TODO: dbg!((curr_row, curr_col, curr_facing, &step));
                     if curr_col < self.rows[curr_row].start_col || curr_col >= self.rows[curr_row].end_col {
                         panic!("outside the field at #{curr_row}, #{curr_col}, #{curr_facing:#?}.")
                     }
@@ -395,6 +275,245 @@ fn p1(board: &Board) -> usize {
     (row + 1) * 1000 + (col + 1) * 4 + (facing as usize)
 }
 
+// --------------------------
+// ********* Part 2 *********
+// --------------------------
+
+/// left, right, up, and down show transiations from the current face borders.
+/// `rows` and `cols` map relative face coordinates to the oritinal net coordinates
+#[derive(Debug, Clone)]
+struct Face {
+    id: usize,
+    rows: Range<usize>,
+    cols: Range<usize>,
+    left: Transition,
+    right: Transition,
+    up: Transition,
+    down: Transition,
+}
+
+/// Transiations are expressed in relative coordinates of the source & destination cube faces.
+/// `swap` is `true` when we need to swap cols & rows coordinates, i.e. (2, 3) becomes (3, 2)
+/// `inv_row` and `inv_col` are applied after `swap`.
+/// if both `inv_row` and `inv_col` are `false`, coordinates are left intact.
+/// if `inv_row` is `true` the row will be inverted, i.e. it will be set to (MAX_ROW - row_id - 1).
+/// `inv_col` does the same for the column.
+#[derive(Debug, Clone, Copy)]
+struct Transition {
+    face_id: usize,
+    facing: Facing,
+    swap: bool,
+    inv_row: bool,
+    inv_col: bool,
+}
+
+/// returns (new_face_id, new_facing, new_face_row_id, new_face_col_id)
+fn move_to_face(
+    faces: &[Face; 6],
+    size: usize,
+    from_face_id: usize,
+    facing: Facing,
+    row_id: usize,
+    col_id: usize,
+) -> (usize, Facing, usize, usize) {
+    let transition = match facing {
+        Left => faces[from_face_id].left,
+        Right => faces[from_face_id].right,
+        Down => faces[from_face_id].down,
+        Up => faces[from_face_id].up,
+    };
+
+    let mut row_id = row_id;
+    let mut col_id = col_id;
+
+    if transition.swap {
+        std::mem::swap(&mut row_id, &mut col_id);
+    }
+
+    if transition.inv_row {
+        row_id = size - row_id - 1;
+    }
+
+    if transition.inv_col {
+        col_id = size - col_id - 1;
+    }
+
+    (transition.face_id, transition.facing, row_id, col_id)
+}
+
+/// Returns (face_id, face_row_id, face_col_id).
+fn board_coords_to_face_coords(faces: &[Face; 6], row_id: usize, col_id: usize) -> (usize, usize, usize) {
+    let face_id = faces.iter().position(|face| face.rows.contains(&row_id) && face.cols.contains(&col_id)).unwrap();
+    let face_row_id = row_id - faces[face_id].rows.start;
+    let face_col_id = col_id - faces[face_id].cols.start;
+    (face_id, face_row_id, face_col_id)
+}
+
+/// Returns a vector, where index is the face_id, and values are (face_row_id, face_col_id) tuples.
+fn get_walls_on_cube(board: &Board, faces: &[Face; 6]) -> Vec<HashSet<(usize, usize)>> {
+    let mut walls = vec![HashSet::new(); 6];
+
+    for (row_id, row) in board.rows.iter().enumerate() {
+        for &col_id in row.walls.iter() {
+            let (face_id, face_row_id, face_col_id) = board_coords_to_face_coords(faces, row_id, col_id);
+            walls[face_id].insert((face_row_id, face_col_id));
+        }
+    }
+
+    walls
+}
+
+fn walk_on_cube(board: &Board, faces: &[Face; 6], size: usize) -> (usize, usize, Facing) {
+    let cube_walls = get_walls_on_cube(board, faces);
+
+    let mut curr_face = 0;
+    let mut curr_row = 0;
+    let mut curr_col = 0;
+    let mut curr_facing = Right;
+
+    // let mut trace = HashMap::new();
+
+    for &step in &board.path {
+        match step {
+            Step::Move { tiles } => {
+                if curr_row >= size || curr_col >= size {
+                    panic!("outside the field at #{curr_face}, #{curr_row}, #{curr_col}, #{curr_facing:#?}.")
+                }
+
+                if cube_walls[curr_face].contains(&(curr_row, curr_col)) {
+                    panic!("ran into a wall at #{curr_face}, #{curr_row}, #{curr_col}, #{curr_facing:#?}.")
+                }
+
+                for _ in 0..tiles {
+                    let mut new_face = curr_face;
+                    let mut new_row = curr_row;
+                    let mut new_col = curr_col;
+                    let mut new_facing = curr_facing;
+
+                    match curr_facing {
+                        Right => {
+                            if curr_col + 1 >= size {
+                                (new_face, new_facing, new_row, new_col) =
+                                    move_to_face(faces, size, curr_face, curr_facing, curr_row, curr_col);
+                            } else {
+                                new_col += 1;
+                            }
+                        }
+                        Left => {
+                            if curr_col == 0 {
+                                (new_face, new_facing, new_row, new_col) =
+                                    move_to_face(faces, size, curr_face, curr_facing, curr_row, curr_col);
+                            } else {
+                                new_col -= 1;
+                            }
+                        }
+                        Down => {
+                            if curr_row + 1 >= size {
+                                (new_face, new_facing, new_row, new_col) =
+                                    move_to_face(faces, size, curr_face, curr_facing, curr_row, curr_col);
+                            } else {
+                                new_row += 1;
+                            }
+                        }
+                        Up => {
+                            if curr_row == 0 {
+                                (new_face, new_facing, new_row, new_col) =
+                                    move_to_face(faces, size, curr_face, curr_facing, curr_row, curr_col);
+                            } else {
+                                new_row -= 1;
+                            }
+                        }
+                    }
+
+                    if !cube_walls[new_face].contains(&(new_row, new_col)) {
+                        curr_face = new_face;
+                        curr_row = new_row;
+                        curr_col = new_col;
+                        curr_facing = new_facing;
+                        // trace.insert((curr_row, curr_col), curr_facing);
+                    }
+                }
+            }
+            Step::TurnL => {
+                curr_facing = curr_facing.turn_l();
+                // trace.insert((curr_row, curr_col), curr_facing);
+            }
+            Step::TurnR => {
+                curr_facing = curr_facing.turn_r();
+                // trace.insert((curr_row, curr_col), curr_facing);
+            }
+        }
+    }
+
+    // print_trace(board, trace);
+
+    (faces[curr_face].rows.start + curr_row, faces[curr_face].cols.start + curr_col, curr_facing)
+}
+
+lazy_static! {
+    static ref TEST_FACES: [Face; 6] = [
+        Face {
+            id: 0,
+            rows: 0..4,
+            cols: 8..12,
+            left: Transition { face_id: 2, facing: Down, swap: true, inv_row: false, inv_col: false },
+            right: Transition { face_id: 5, facing: Left, swap: false, inv_row: true, inv_col: false },
+            up: Transition { face_id: 1, facing: Down, swap: false, inv_row: false, inv_col: true },
+            down: Transition { face_id: 3, facing: Down, swap: false, inv_row: true, inv_col: false },
+        },
+        Face {
+            id: 1,
+            rows: 4..8,
+            cols: 0..4,
+            left: Transition { face_id: 5, facing: Up, swap: true, inv_row: true, inv_col: true },
+            right: Transition { face_id: 2, facing: Right, swap: false, inv_row: false, inv_col: true },
+            up: Transition { face_id: 0, facing: Down, swap: false, inv_row: false, inv_col: true },
+            down: Transition { face_id: 4, facing: Up, swap: false, inv_row: false, inv_col: true }
+        },
+        Face {
+            id: 2,
+            rows: 4..8,
+            cols: 4..8,
+            left: Transition { face_id: 1, facing: Left, swap: false, inv_row: false, inv_col: true },
+            right: Transition { face_id: 3, facing: Right, swap: false, inv_row: false, inv_col: true },
+            up: Transition { face_id: 0, facing: Right, swap: true, inv_row: false, inv_col: false },
+            down: Transition { face_id: 4, facing: Right, swap: true, inv_row: true, inv_col: true }
+        },
+        Face {
+            id: 3,
+            rows: 4..8,
+            cols: 8..12,
+            left: Transition { face_id: 2, facing: Left, swap: false, inv_row: false, inv_col: true },
+            right: Transition { face_id: 5, facing: Down, swap: true, inv_row: true, inv_col: true },
+            up: Transition { face_id: 0, facing: Up, swap: false, inv_row: true, inv_col: false },
+            down: Transition { face_id: 4, facing: Down, swap: false, inv_row: true, inv_col: false }
+        },
+        Face {
+            id: 4,
+            rows: 8..12,
+            cols: 8..12,
+            left: Transition { face_id: 2, facing: Up, swap: true, inv_row: true, inv_col: true },
+            right: Transition { face_id: 5, facing: Right, swap: false, inv_row: false, inv_col: true },
+            up: Transition { face_id: 3, facing: Up, swap: false, inv_row: true, inv_col: false },
+            down: Transition { face_id: 1, facing: Up, swap: false, inv_row: false, inv_col: true }
+        },
+        Face {
+            id: 5,
+            rows: 8..12,
+            cols: 12..16,
+            left: Transition { face_id: 4, facing: Left, swap: false, inv_row: false, inv_col: true },
+            right: Transition { face_id: 0, facing: Left, swap: false, inv_row: true, inv_col: false },
+            up: Transition { face_id: 3, facing: Right, swap: true, inv_row: true, inv_col: true },
+            down: Transition { face_id: 1, facing: Right, swap: true, inv_row: true, inv_col: true }
+        }
+    ];
+}
+
+fn p2(board: &Board, faces: &[Face; 6], size: usize) -> usize {
+    let (row, col, facing) = walk_on_cube(board, faces, size);
+    (row + 1) * 1000 + (col + 1) * 4 + (facing as usize)
+}
+
 fn main() {
     let input = parse_input("../inputs/d22");
 
@@ -450,5 +569,36 @@ mod tests {
         assert_eq!(move_to_face(&TEST_FACES, size, 5, Right, 1, 3), (0, Left, 2, 3));
         assert_eq!(move_to_face(&TEST_FACES, size, 5, Up, 0, 1), (3, Right, 2, 3));
         assert_eq!(move_to_face(&TEST_FACES, size, 5, Down, 3, 1), (1, Right, 2, 0));
+    }
+
+    #[test]
+    fn board_coords_to_face_coords_test() {
+        let test_input = parse_input("../inputs/d22_test");
+
+        assert_eq!(board_coords_to_face_coords(&TEST_FACES, 0, test_input.rows[0].start_col), (0, 0, 0));
+        assert_eq!(board_coords_to_face_coords(&TEST_FACES, 0, test_input.rows[0].start_col + 1), (0, 0, 1));
+        assert_eq!(board_coords_to_face_coords(&TEST_FACES, 1, test_input.rows[0].start_col), (0, 1, 0));
+        assert_eq!(board_coords_to_face_coords(&TEST_FACES, 5, 7), (2, 1, 3));
+    }
+
+    #[test]
+    fn get_walls_on_cube_test() {
+        let test_input = parse_input("../inputs/d22_test");
+
+        let cube_walls = get_walls_on_cube(&test_input, &TEST_FACES);
+        assert_eq!(cube_walls.len(), 6);
+
+        let second_face_walls = &cube_walls[2];
+        assert_eq!(second_face_walls.len(), 1);
+        assert!(second_face_walls.contains(&(2, 3)));
+    }
+
+    #[test]
+    fn p2_test() {
+        let test_input = parse_input("../inputs/d22_test");
+        assert_eq!(p2(&test_input, &TEST_FACES, 4), 5031);
+
+        // let input = parse_input("../inputs/d22");
+        // assert_eq!(p2(&input), 89224);
     }
 }
